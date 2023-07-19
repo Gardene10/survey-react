783d3a369a2dfd67b58c5341cb881ef9fdb36723
Gardene 10 <gardene.v.c@gmail.com>
2023-07-19 10:42:49 -0300

    feat: ensure RA throws UnexpectedError on default cases

--- src/data/usecases/authentication/remote-authentiction.ts
+++ src/data/usecases/authentication/remote-authentiction.ts
@@ -1,21 +1,26 @@
+
+// classe de produçao
+
 import { HttpPostClient } from "@/data/protocols/http/http-post-client";
 import { HttpStatusCode } from "@/data/protocols/http/http-response";
 import { InvalidCredentialsError } from "@/domain/errors/Invalid-credentials-error";
+import { UnexpectedError } from "@/domain/errors/unexpected-error";
 import { AuthenticationParams } from "@/domain/usecases/authentication";
 
 export class RemoteAuthentication {
     constructor  (
         private readonly url: string,
         private readonly httpPostClient: HttpPostClient){}
-        
+
     async auth(params: AuthenticationParams): Promise<void>{
         const httpResponse = await this.httpPostClient.post({
             url: this.url,
             body: params
         })
         switch (httpResponse.statusCode) {
+            case HttpStatusCode.ok : break
             case HttpStatusCode.unathorized: throw new InvalidCredentialsError()
-            default: return Promise.resolve()
+            default: throw new UnexpectedError()
         }
     }
 }
