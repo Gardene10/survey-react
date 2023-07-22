c7cac15f6c1087496af2d056dcef385998cb373f
Gardene 10 <gardene.v.c@gmail.com>
2023-07-22 10:06:32 -0300

    refactor: move FormStaus to a separate file

--- src/presentention/components/login-header/login-header.tsx
+++ src/presentention/components/login-header/login-header.tsx
@@ -3,10 +3,6 @@ import React, {memo} from "react";
 import  Styles  from "./login-header-styles.scss";
 import Logo from "@/presentention/components/logo/logo";
 
-
-
-
-
 const LoginHeader: React.FC = () => {
     return (
         <header className={Styles.header}>
